c5d3c91d219d1ad6d15cb7684cb4f80c00525a2f
RaviPratihast <ravipratihast@gmail.com>
2023-06-13 20:47:16 +0530

    chore: created cart and wishlist page.

--- src/pages/page-index.js
+++ src/pages/page-index.js
@@ -1,4 +1,5 @@
 import { Home } from "./home-page/home";
 import { Shop } from "./shop-page/shop";
+import { Wishlist } from "./wishlist-page/wishlist";
 
-export { Home, Shop };
+export { Home, Shop, Wishlist };
