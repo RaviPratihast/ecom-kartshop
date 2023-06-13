5f2ec233a3470665d51b44fede9ce6230af78245
RaviPratihast <ravipratihast@gmail.com>
2023-06-13 21:51:17 +0530

    feat: Routing of pages initiated.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import "./App.css";
-import { Navigation, Button, Card } from "./components/component-index";
+import { NavLink, Routes, Route } from "react-router-dom";
+// import { Navigation } from "./components/component-index";
+import { Cart, Home, Login, Shop, SignIn, Wishlist } from "./pages/page-index";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <Navigation />
-        <Button />
-        <Card />
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/shop">Shop</NavLink>
+          <NavLink to="/wishlist">Wishlist</NavLink>
+          <NavLink to="/cart">Cart</NavLink>
+        </nav>
       </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signIn" element={<SignIn />} />
+        </Routes>
+      </main>
     </div>
   );
 }
