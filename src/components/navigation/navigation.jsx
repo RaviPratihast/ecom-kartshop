79022837b7f44d62fa2936c2a59b1aaadbcd19a9
RaviPratihast <ravipratihast@gmail.com>
2023-06-13 23:07:43 +0530

    chore: consolidated css files & optimized imports.

--- src/components/navigation/navigation.jsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom";
-
-// const Navigation = ({ children, onClick, className }) => {
-//   return <NavLink>{children}</NavLink>;
-// };
-
-// export { Navigation };
