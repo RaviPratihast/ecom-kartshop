d0c88b0b662d72dfb0d0d6afd4bed5fc6557f47c
RaviPratihast <ravipratihast@gmail.com>
2023-06-18 17:11:16 +0530

    chore: home page style done.

--- src/pages/home-page/home.jsx
+++ src/pages/home-page/home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Button } from "../../components/component-index";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className="home-page">
+      <div className="image-container">
+        <img
+          src={process.env.PUBLIC_URL + "/image/empty.jpg"}
+          alt="shopping cart"
+          className="img home-img"
+        />
+        <div className="explore-container">
+          <div className="explore-container-text">
+            <h1>
+              Kart<span>Shop</span>
+            </h1>
+            <p>Shop everything you want.</p>
+          </div>
+          <Button>Explore more</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { Home };
