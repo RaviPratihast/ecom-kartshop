1d4843efdbe0b9fb8c6f453884b9995d9b54411f
RaviPratihast <ravipratihast@gmail.com>
2023-06-19 21:52:28 +0530

    chore: wishlist component added and styled.

--- src/pages/wishlist-page/wishlist.jsx
+++ src/pages/wishlist-page/wishlist.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useProduct } from "../../context/ecom-context";
+import { Button, Card } from "../../components/component-index";
 
 const Wishlist = () => {
-  return <div>Wishlist</div>;
+  const { state, dispatch } = useProduct();
+  return (
+    <div className="wishlist-container">
+      <div className="wishlist-count">
+        <h3>items : 1</h3>
+      </div>
+      <div className="wishlist-items-container">
+        {state.product.map(
+          ({ id, name, rating, image, originalPrice, price }) => {
+            return (
+              <Card
+                key={id}
+                id={id}
+                name={name}
+                rating={rating}
+                image={image}
+                originalPrice={originalPrice}
+                price={price}
+                label="product"
+              >
+                <div className="add-cart-wishlist-container">
+                  <Button onClick={() => console.log("hey")}>
+                    Add To Cart
+                  </Button>
+                  <Button onClick={() => console.log("hey")}>
+                    <span class="material-icons favorite-icon">favorite</span>
+                  </Button>
+                </div>
+              </Card>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
 };
 
 export { Wishlist };
