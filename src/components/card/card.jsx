7c88e0a806c684fec62fcd470062a85e342a0f5d
RaviPratihast <ravipratihast@gmail.com>
2023-06-19 10:08:32 +0530

    chore: implement the card component.

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({
+  id,
+  name,
+  rating,
+  image,
+  originalPrice,
+  price,
+  qty,
+  label,
+  children,
+}) => {
+  return (
+    <div className="card">
+      <div className="image-container">
+        <img src={image} alt={name} className="card-image" />
+      </div>
+      <div className="card-content">
+        <div className="name-rating-container">
+          <div className="name-container">
+            <h3 className="card-name">{name}</h3>
+          </div>
+          <div className="ratings-container">
+            <i class="material-icons">grade</i>
+            <span className="card-rating">{rating}</span>
+          </div>
+        </div>
+        <div className="card-details">
+          <p>
+            Rs <span className="sale-price">{price}</span>
+          </p>
+
+          <span className="original-price">Rs {originalPrice}</span>
+          <span className="off-percent">10% off</span>
+        </div>
+        <div className="buttons-add-to-cart-wish-container">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export { Card };
