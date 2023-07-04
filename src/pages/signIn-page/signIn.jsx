9d2f89c2eb3a990a558a7ce06fe107250a0fbca2
RaviPratihast <ravipratihast@gmail.com>
2023-07-04 13:31:05 +0530

    feat: sign in UI completed

--- src/pages/signIn-page/signIn.jsx
+++ src/pages/signIn-page/signIn.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Button, Card } from "../../components/component-index";
 const SignIn = () => {
-  return <div>sign up</div>;
+  return (
+    <div className="sign-in-container">
+      <h2>Sign Up</h2>
+      <div className="input-container">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+      </div>
+      <div className="input-button-container">
+        <Button>Sign In</Button>
+      </div>
+      <div className="have-account-login-container">
+        <p>Already have account? </p>
+        <span>Login</span>
+      </div>
+    </div>
+  );
 };
 
 export { SignIn };
