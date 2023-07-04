882f5973dc07c8ac72deefed196b0598fe84ecdc
RaviPratihast <ravipratihast@gmail.com>
2023-07-04 13:41:38 +0530

    feat: login UI is completed.

--- src/pages/login-page/login.jsx
+++ src/pages/login-page/login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { Button, Card } from "../../components/component-index";
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div className="Login-container">
+      <h2>Log In</h2>
+      <div className="input-container">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        {/* <input type="password" placeholder="Confirm Password" /> */}
+      </div>
+      <div className="input-button-container">
+        <Button>Log In</Button>
+        <Button>Guest Login</Button>
+      </div>
+      <div className="have-account-login-container">
+        <p>Don't have account? </p>
+        <span>Sign In</span>
+      </div>
+    </div>
+  );
 };
 
 export { Login };
