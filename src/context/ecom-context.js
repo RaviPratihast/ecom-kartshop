92ba876630c80b25e01313b512172a04ff7e4443
RaviPratihast <ravipratihast@gmail.com>
2023-08-10 13:44:45 +0530

    Refactor: removed commented code.

--- src/context/ecom-context.js
+++ src/context/ecom-context.js
@@ -4,28 +4,18 @@ import {
   useEffect,
   useState,
   useReducer,
-  //   useState,
-  //   useEffect,
 } from "react";
-// import axios from "axios";
 import { items } from "../items";
 import { reducer } from "../reducers/reducer";
-
-// import { reducer } from "../reducers/reducer";
-
-// creating the context
 const ProductContext = createContext(null);
-// using the context abstracting it from the normal user.
 const useProduct = () => useContext(ProductContext);
 let initialState = {
   initialProduct: items,
   product: items,
   filteredProduct: [],
-  // filterApplied: false,
   rangeFilterApplied: false,
   ratingFilterApplied: false,
   cart: [],
-  // cart:{cartItems:[],totalPriceOfCartItems:0},
   wishlist: [],
   subTotalOfCart: 0,
   minPrice: 0,
