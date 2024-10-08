5eb59391d1ba2808c6def3615384b2f92843a2a5
Ravi Pratihast <ravipratihast@gmail.com>
2024-10-09 01:18:34 +0530

    style: changes in navbar

--- src/pages/home-page/home.jsx
+++ src/pages/home-page/home.jsx
@@ -5,21 +5,38 @@ import { Button } from "../../components/component-index";
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className="home-page">
-      <div className="image-container">
-        <img
-          src={process.env.PUBLIC_URL + "/image/empty.jpg"}
-          alt="shopping cart"
-          className="img home-img"
-        />
-        <div className="explore-container">
-          <div className="explore-container-text">
-            <h1>
-              Kart<span>Shop</span>
-            </h1>
-            <p>Shop everything you want.</p>
-          </div>
+    // <div className="home-page">
+    //   <div className="explore-container">
+    //     <div className="explore-container-text">
+    //       <h1>
+    //         Kart<span>Shop</span>
+    //       </h1>
+    //       <p>Shop everything you want.</p>
+    //     </div>
+    //     <Button onClick={() => navigate("/shop")}>Explore more</Button>
+    //   </div>
+    //   <div className="image-container">
+    //     <img
+    //       src="/image/empty-cart.svg"
+    //       alt="shopping cart"
+    //       className="img home-img"
+    //     />
+    //   </div>
+    // </div>
+    <div>
+      <div className="home-wrapper">
+        <div className="home-content">
+          <h1>Kartshop</h1>
+          <h3>Shop everything you want</h3>
           <Button onClick={() => navigate("/shop")}>Explore more</Button>
+        </div>
+
+        <div className="home-image">
+          <img
+            className="image-responsive"
+            src="/image/empty-cart.svg"
+            alt="home-hero"
+          />
         </div>
       </div>
     </div>
